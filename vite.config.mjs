20a9b4aac14ddd581a164428ed9341bee962c1a2
Ana Maria Souza <anamariasouza701@gmail.com>
2025-01-13 15:37:31 -0400

    transformando projeto em PWA

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ import Layouts from 'vite-plugin-vue-layouts'
 import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -19,6 +20,7 @@ export default defineConfig({
     Vue({
       template: { transformAssetUrls }
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify({
       autoImport: true,
